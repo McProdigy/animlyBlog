c981d70749aab2891ba1bb0781b50ea6467e0f5a
McProdigy <50849359+McProdigy@users.noreply.github.com>
2021-10-22 11:26:03 +0300

    'a'

--- animly/settings.py
+++ animly/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jd3e_09_*ur-v=tc=#@(jy@@i1p20$9kr_+mb_=v+ia39ol8-=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://fierce-cove-74909.herokuapp.com']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
